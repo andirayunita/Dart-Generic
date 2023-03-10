44e824c1e644e049f59c1084b29865bf1fb2fd8c
Andira Yunita <andhirayp@gmail.com>
2023-03-10 14:28:09 +0700

    First Commit

---
+++ bin/study_dart_generic.dart
@@ -0,0 +1,5 @@
+import 'package:study_dart_generic/study_dart_generic.dart' as study_dart_generic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_dart_generic.calculate()}!');
+}
---
+++ test/study_dart_generic_test.dart
@@ -0,0 +1,8 @@
+import 'package:study_dart_generic/study_dart_generic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
